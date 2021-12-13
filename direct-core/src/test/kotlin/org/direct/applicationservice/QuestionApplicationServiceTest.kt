f50e999eb642fc91a65fa939c76fa775d2c425c6
BooookStore <6366129+BooookStore@users.noreply.github.com>
2021-12-13 22:35:40 +0900

    test: add `user can close question`

--- direct-core/src/test/kotlin/org/direct/applicationservice/QuestionApplicationServiceTest.kt
+++ direct-core/src/test/kotlin/org/direct/applicationservice/QuestionApplicationServiceTest.kt
@@ -1,6 +1,7 @@
 package org.direct.applicationservice
 
 import org.assertj.core.api.Assertions.assertThat
+import org.direct.domain.question.QuestionStatus.CLOSED
 import org.direct.domain.user.UserId
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -35,6 +36,27 @@ internal class QuestionApplicationServiceTest : ApplicationServiceTestSupport() 
             assertThat(it?.title).isEqualTo("how install Apache Maven ?")
             assertThat(it?.subject).isEqualTo("I want to install Apache Maven.")
             assertThat(it?.questioner).isEqualTo(UserId("USER1"))
+        }
+    }
+
+    @Test
+    fun `user can close question`() {
+        // setup
+        // create new question
+        val newQuestionId = questionApplicationService.newQuestion(
+            QuestionNewCommand(
+                title = "how install Apache Maven ?",
+                subject = "I want to install Apache Maven.",
+                questionerUserId = "USER1",
+            )
+        )
+
+        // execute
+        questionApplicationService.closeQuestion(newQuestionId.rawId)
+
+        // verify
+        inMemoryQuestionRepository().entities[newQuestionId].let {
+            assertThat(it?.status).isEqualTo(CLOSED)
         }
     }
 

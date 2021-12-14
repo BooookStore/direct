e86a239826715dc1f2a6af3eb0b3a66f96e7c58a
BooookStore <6366129+BooookStore@users.noreply.github.com>
2021-12-14 20:43:18 +0900

    feature: add User and UserRepository

---
+++ direct-core/src/main/kotlin/org/direct/domain/user/User.kt
@@ -0,0 +1,5 @@
+@file:Suppress("unused")
+
+package org.direct.domain.user
+
+class User(val id: UserId)
---
+++ direct-core/src/main/kotlin/org/direct/domain/user/UserRepository.kt
@@ -0,0 +1,11 @@
+@file:Suppress("unused")
+
+package org.direct.domain.user
+
+interface UserRepository {
+
+    fun save(user: User)
+
+    fun findById(userId: UserId): User?
+
+}

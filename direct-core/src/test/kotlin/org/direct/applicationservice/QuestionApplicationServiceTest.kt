68ec2d83bf8f9bde45eaf301e712eb3423b9daeb
BooookStore <6366129+BooookStore@users.noreply.github.com>
2021-12-14 21:14:42 +0900

    test: add test

--- direct-core/src/test/kotlin/org/direct/applicationservice/QuestionApplicationServiceTest.kt
+++ direct-core/src/test/kotlin/org/direct/applicationservice/QuestionApplicationServiceTest.kt
@@ -1,6 +1,7 @@
 package org.direct.applicationservice
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.direct.domain.question.QuestionStatus.CLOSED
 import org.direct.domain.user.User
 import org.direct.domain.user.UserId
@@ -41,6 +42,20 @@ internal class QuestionApplicationServiceTest : ApplicationServiceTestSupport() 
             assertThat(it?.subject).isEqualTo("I want to install Apache Maven.")
             assertThat(it?.questioner).isEqualTo(UserId("USER1"))
         }
+    }
+
+    @Test
+    fun `can not create new question without user`() {
+        // setup
+        val command = QuestionNewCommand(
+            title = "how install Apache Maven ?",
+            subject = "I want to install Apache Maven.",
+            questionerUserId = "DONT EXIST USER !",
+        )
+
+        // execute & verify
+        assertThatThrownBy { questionApplicationService.newQuestion(command) }
+            .isExactlyInstanceOf(IllegalArgumentException::class.java)
     }
 
     @Test

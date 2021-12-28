fd1aab10615f40ffb433cc1aba9e63a4ff037576
BooookStore <6366129+BooookStore@users.noreply.github.com>
2021-12-28 19:09:40 +0900

    refactoring: change method name

--- direct-core/src/main/kotlin/org/direct/domain/question/Question.kt
+++ direct-core/src/main/kotlin/org/direct/domain/question/Question.kt
@@ -4,10 +4,10 @@ package org.direct.domain.question
 
 import org.direct.domain.DomainException
 import org.direct.domain.answer.AnswerId
-import org.direct.domain.question.QuestionAuthorityPolicy.canDelete
-import org.direct.domain.question.QuestionAuthorityPolicy.canEdit
-import org.direct.domain.question.QuestionAuthorityPolicy.canPublic
-import org.direct.domain.question.QuestionAuthorityPolicy.canResolve
+import org.direct.domain.question.QuestionAuthorityPolicy.allowDelete
+import org.direct.domain.question.QuestionAuthorityPolicy.allowResolve
+import org.direct.domain.question.QuestionAuthorityPolicy.allowedEdit
+import org.direct.domain.question.QuestionAuthorityPolicy.allowedPublic
 import org.direct.domain.question.QuestionVisibility.*
 import org.direct.domain.question.QuestionVisibilityAndResolveStatusPolicy.canCombine
 import org.direct.domain.user.User
@@ -61,35 +61,35 @@ class Question(
         private set
 
     fun editTitle(newTitle: String, editUser: User) {
-        if ((editUser canEdit this).not()) throw DomainException("user not allowed edit : userId=[${editUser.id.rawId}] questionId=[${id.rawId}]")
+        if ((editUser allowedEdit this).not()) throw DomainException("user not allowed edit : userId=[${editUser.id.rawId}] questionId=[${id.rawId}]")
         if (visibility == DELETED) throw DomainException("question already deleted : questionId=[${id.rawId}]")
 
         title = newTitle
     }
 
     fun editSubject(newSubject: String, editUser: User) {
-        if ((editUser canEdit this).not()) throw DomainException("user not allowed edit : userId=[${editUser.id.rawId}] questionId=[${id.rawId}]")
+        if ((editUser allowedEdit this).not()) throw DomainException("user not allowed edit : userId=[${editUser.id.rawId}] questionId=[${id.rawId}]")
         if (visibility == DELETED) throw DomainException("question already deleted : questionId=[${id.rawId}]")
 
         subject = newSubject
     }
 
     fun public(operateUser: User) {
-        if ((operateUser canPublic this).not()) throw DomainException("user not allowed public : userId=[${operateUser.id.rawId}] questionId=[${id.rawId}]")
+        if ((operateUser allowedPublic this).not()) throw DomainException("user not allowed public : userId=[${operateUser.id.rawId}] questionId=[${id.rawId}]")
         if ((resolveStatus canCombine PUBLIC).not()) throw DomainException("can't public question : questionId=[${id.rawId}]")
 
         visibility = visibility.public()
     }
 
     fun delete(operateUser: User) {
-        if ((operateUser canDelete this).not()) throw DomainException("user not allowd delete : userId=[${operateUser.id.rawId}] questionId=[${id.rawId}]")
+        if ((operateUser allowDelete this).not()) throw DomainException("user not allowd delete : userId=[${operateUser.id.rawId}] questionId=[${id.rawId}]")
         if ((resolveStatus canCombine DELETED).not()) throw DomainException("can't delete question : questionId=[${id.rawId}]")
 
         visibility = visibility.delete()
     }
 
     fun resolve(resolvedAnswerId: AnswerId, operateUser: User) {
-        if ((operateUser canResolve this).not()) throw DomainException("user not allowd resolve : userId=[${operateUser.id.rawId}] questionId=[${id.rawId}]")
+        if ((operateUser allowResolve this).not()) throw DomainException("user not allowd resolve : userId=[${operateUser.id.rawId}] questionId=[${id.rawId}]")
         if ((visibility canCombine QuestionResolved(resolvedAnswerId)).not()) throw DomainException("can't question to resolved : questionId=[${id.rawId}]")
 
         resolveStatus = resolveStatus.toResolved(resolvedAnswerId)
--- direct-core/src/main/kotlin/org/direct/domain/question/QuestionAuthorityPolicy.kt
+++ direct-core/src/main/kotlin/org/direct/domain/question/QuestionAuthorityPolicy.kt
@@ -5,25 +5,25 @@ import org.direct.domain.user.UserId
 
 object QuestionAuthorityPolicy {
 
-    infix fun User.canEdit(question: Question): Boolean = when {
+    infix fun User.allowedEdit(question: Question): Boolean = when {
         isQuestioner(question.questioner) -> true
         isAuditor() -> true
         else -> false
     }
 
-    infix fun User.canPublic(question: Question): Boolean = when {
+    infix fun User.allowedPublic(question: Question): Boolean = when {
         isQuestioner(question.questioner) -> true
         isAuditor() -> true
         else -> false
     }
 
-    infix fun User.canDelete(question: Question): Boolean = when {
+    infix fun User.allowDelete(question: Question): Boolean = when {
         isQuestioner(question.questioner) -> true
         isAuditor() -> true
         else -> false
     }
 
-    infix fun User.canResolve(question: Question): Boolean = isQuestioner(question.questioner)
+    infix fun User.allowResolve(question: Question): Boolean = isQuestioner(question.questioner)
 
     private fun User.isQuestioner(questioner: UserId) = questioner == id
 

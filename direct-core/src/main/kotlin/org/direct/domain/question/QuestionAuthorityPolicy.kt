889d7fd587aa016f80ec5bdb52842baa6b59caa4
BooookStore <6366129+BooookStore@users.noreply.github.com>
2021-12-28 19:11:05 +0900

    refactoring: QuestionAuthorityPolicy.kt

--- direct-core/src/main/kotlin/org/direct/domain/question/QuestionAuthorityPolicy.kt
+++ direct-core/src/main/kotlin/org/direct/domain/question/QuestionAuthorityPolicy.kt
@@ -1,30 +1,29 @@
 package org.direct.domain.question
 
 import org.direct.domain.user.User
-import org.direct.domain.user.UserId
 
 object QuestionAuthorityPolicy {
 
     infix fun User.allowedEdit(question: Question): Boolean = when {
-        isQuestioner(question.questioner) -> true
+        isQuestioner(question) -> true
         isAuditor() -> true
         else -> false
     }
 
     infix fun User.allowedPublic(question: Question): Boolean = when {
-        isQuestioner(question.questioner) -> true
+        isQuestioner(question) -> true
         isAuditor() -> true
         else -> false
     }
 
     infix fun User.allowDelete(question: Question): Boolean = when {
-        isQuestioner(question.questioner) -> true
+        isQuestioner(question) -> true
         isAuditor() -> true
         else -> false
     }
 
-    infix fun User.allowResolve(question: Question): Boolean = isQuestioner(question.questioner)
+    infix fun User.allowResolve(question: Question): Boolean = isQuestioner(question)
 
-    private fun User.isQuestioner(questioner: UserId) = questioner == id
+    private fun User.isQuestioner(question: Question) = question.questioner == id
 
 }
